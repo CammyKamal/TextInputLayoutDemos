e6152ce7e145c2b50b478d640a145378f96b22f6
kamal <kamal.vaid@macys.com>
2020-01-02 00:25:53 +0530

    changes

--- Demo/app/src/main/java/com/kamal/textinputlayoutdemo/MainActivity.kt
+++ Demo/app/src/main/java/com/kamal/textinputlayoutdemo/MainActivity.kt
@@ -6,6 +6,7 @@ import android.text.Html
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
@@ -20,16 +21,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         textInput = findViewById(R.id.textInput) as TextInputLayout
 
-        val wordtoSpan = SpannableString(Html.fromHtml("Hello world <sup>*</sup>"))
+        val wordtoSpan = SpannableString(Html.fromHtml("H<sup>*</sup>"))
         wordtoSpan.setSpan(
-            ForegroundColorSpan(Color.BLUE), 11, 12,
+            ForegroundColorSpan(Color.RED), 1, 2,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
         )
-        textInput!!.hint = wordtoSpan
+        textInput!!.hint = ""
 
         et2 = findViewById(R.id.et2) as TextInputEditText
-        et2!!.hint = wordtoSpan
+       et2!!.hint = wordtoSpan
+
+        et2!!.setOnFocusChangeListener(){ view: View, b: Boolean ->
+
+            if(b){
+                et2!!.hint = ""
+                textInput!!.hint = wordtoSpan
+            }
+        }
 
 
     }
+
+
 }
